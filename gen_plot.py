f93d91edca3fa8218ea348d33b982090c9fd13fe
Marshall Ward <marshall.ward@noaa.gov>
2025-12-16 14:20:02 -0500

    Graph improvements
    Legends etc

--- gen_plot.py
+++ gen_plot.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 # Read files
 
 platforms = ('cpu', 'gpu',)
+#platforms = ('gpu',)
 
 regions = [
     '(Ocean Coriolis & mom advection)',
@@ -16,6 +20,11 @@ regions = [
     '(Ocean barotropic mode stepping)',
 ]
 
+plotcolor = {
+    'gpu': 'orange',
+    'cpu': 'blue',
+}
+
 run_files = {}
 run_files['cpu'] = [run for run in os.listdir() if run.startswith('cpu_')]
 run_files['gpu'] = [run for run in os.listdir() if run.startswith('gpu_')]
@@ -25,46 +34,93 @@ stats = {}
 for expt in platforms:
     data_files = run_files[expt]
 
-    # NOTE: File is (platform, resolution): region: timing
-    #   We want to invert to platform: region: timing: resolution
-    runs = {}
+    # NOTE: File is `(platform, resolution): region: timing`
+    #   We invert to `platform: region: resolution: timing`
+    #   But we may want `platform: region: timing: resolution`
+
     for runfile in data_files:
-        clocks = {}
-        with open(runfile) as timings_file:
-            header = timings_file.readline()
+        resolution = runfile.split('_')[1].split('.')[0].lstrip('0')
+
+        metrics = {}
+        with open(runfile) as stats_file:
+            # Extract headers from first line
+            # TODO: Allow full stdout as input
+            header = stats_file.readline()
             keys = header.split()
 
-            for line in timings_file:
+            for line in stats_file:
+                if line.strip().startswith('MPP_STACK high water mark'):
+                    continue
+
                 rec = line.rsplit(maxsplit=len(keys))
 
                 clk = rec[0]
-                clocks[clk] = {}
-                for key, value in zip(keys, rec[1:]):
-                    clocks[clk][key] = value
+                try:
+                    metrics[clk][resolution] = {}
+                except KeyError:
+                    metrics[clk] = {}
+                    metrics[clk][resolution] = {}
 
-        run_label = runfile.split('_')[1].split('.')[0].lstrip('0')
+                for stat, value in zip(keys, rec[1:]):
+                    metrics[clk][resolution][stat] = float(value)
 
-        runs[run_label] = clocks
-
-    stats[expt] = runs
-
+        # Poor man's deepupdate()
+        # Assumes that all levels exist if `expt` exists.
+        try:
+            for reg in metrics:
+                stats[expt][reg].update(metrics[reg])
+        except KeyError:
+            stats[expt] = metrics
 
 # Plot results
-fig, axes = plt.subplots(2, 3)
+fig, axes = plt.subplots(2, 3, figsize=(12,8))
+
+fig.suptitle('Runtime per step for MOM6 modules')
+
+# Denote the CPU core limit
+for ax in axes.flat:
+    ax.axvline(64, linestyle="--")
 
 for expt in platforms:
-    # Generate x-axis
-    # TODO: Maybe keep as dict?
-    #run_labels = [int(k.rstrip('x')) for k in stats[expt].keys()]
-
-    # Get run keys
-    run_keys = sorted(stats[expt], key=lambda k: int(k.rstrip('x')))
-    print(run_keys)
-
     for reg, ax in zip(regions, axes.flat):
-        #tmin = stats[expt][reg]['tmin']
-        #tavg = stats[expt][reg]['tavg']
-        #tmax = stats[expt][reg]['tmax']
+
+        nx_keys = stats[expt][reg].keys()
+        nx = [int(k.rstrip('x')) for k in nx_keys]
+
+        tmin = np.array([stats[expt][reg][nx]['tmin'] for nx in nx_keys])
+        tmax = np.array([stats[expt][reg][nx]['tmax'] for nx in nx_keys])
+        tavg = np.array([stats[expt][reg][nx]['tavg'] for nx in nx_keys])
+
+        hits = np.array([stats[expt][reg][nx]['hits'] for nx in nx_keys])
 
         ax.set_title(reg)
+
+        # Explicit log ticks
         ax.set_xscale('log')
+        ax.xaxis.set_major_locator(mticker.FixedLocator(nx))
+        ax.xaxis.set_minor_locator(mticker.NullLocator())
+
+        ax.set_xticklabels(nx_keys)
+
+        ax.grid(True, linestyle=':', linewidth=0.5, alpha=1.0)
+
+        ax.plot(nx, tavg / hits, '-', color=plotcolor[expt],
+            label=expt.upper() + " tavg")
+        ax.plot(nx, tmax / hits, '--', color=plotcolor[expt],
+            label=expt.upper() + " tmax")
+        ax.plot(nx, tmin / hits, ':', color=plotcolor[expt],
+            label=expt.upper() + " tmin")
+
+        ax.plot(nx, tavg / hits, 'o', color=plotcolor[expt])
+        ax.plot(nx, tmax / hits, 'o', color=plotcolor[expt])
+        ax.plot(nx, tmin / hits, 'o', color=plotcolor[expt])
+
+        # Adjust ranges
+        if reg == '(Ocean continuity equation)':
+            ax.set_ylim([0.0, 0.06])
+        if reg == '(Ocean barotropic mode stepping)':
+            ax.set_ylim([0.0, 0.03])
+
+axes[0,0].legend()
+
+plt.show()
