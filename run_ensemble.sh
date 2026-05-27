e70dd64a5f52e8930202fc20eb4cc782d577962a
Marshall Ward <marshall.ward@noaa.gov>
2026-05-27 13:21:40 -0400

    Update run_ensemble
    Using the newer run_ensemble script with GPU domain setup and a few
    other things.
    
    Also all of the junk baggage for launching the job in other other
    environments.

--- run_ensemble.sh
+++ run_ensemble.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+MOM6_EXEC=../build_arm/MOM6
+
+# This is needed for some environments
+first=$(cat /proc/self/status | grep Cpus_allowed_list | cut -f 2 | cut -d'-' -f 1)
+
 JOBSIZES="1 2 4 8 16 32 64 128"
 #JOBSIZES="1 2 4 8 16 32 64 96 128"
 #JOBSIZES="1 2 4 8 16 32 64 128 256 512"
@@ -9,54 +14,52 @@ CPU_PER_NODE=96
 
 # Set to cpu or gpu
 PLATFORM=gpu
+NGPUS=1
 
 #---
 
-cpuid_max=$(( ${CPU_PER_NODE} - 1 ))
+#cpuid_max=$(( ${CPU_PER_NODE} - 1 ))
 
 # Construct a square-like layout m x n for i ranks
 get_layout() {
- 	local i=$1
-  	m=1
+    local i=$1
+    m=1
 
-	# Find the smallest m such that m**2 > i
-  	while (( (m+1)*(m+1) <= i )); do
-    	((m++))
-  	done
+    # Find the smallest m such that m**2 > i
+    while (( (m+1)*(m+1) <= i )); do
+        ((m++))
+    done
 
-	# Then decrement m until it exactly divides i
-  	while (( i % m != 0 )); do
-  	  ((m--))
-  	done
+    # Then decrement m until it exactly divides i
+    while (( i % m != 0 )); do
+        ((m--))
+    done
 
-	# Finally, set n such that m*n == i
-  	n=$(( i / m ))
+    # Finally, set n such that m*n == i
+    n=$(( i / m ))
 
-	# Force m >= n
-  	if (( m < n )); then
-  	  	local t=$m
-  	  	m=$n
-  	  	n=$t
-  	fi
+    # Force m >= n
+    if (( m < n )); then
+        local t=$m
+        m=$n
+        n=$t
+    fi
 }
 
 for i in ${JOBSIZES}; do
-    k=0
-    t=$i
-    while (( t > 1 )); do
-        ((k++))
-        t=$((t >> 1))
-    done
-
-	get_layout "$i"
-
     if [ "$PLATFORM" = "gpu" ]; then
-        lx=1
-        ly=1
+	   get_layout "${NGPUS}"
     else
-	    lx=${m}
-	    ly=${n}
+        nranks=$(( i > CPU_PER_NODE ? CPU_PER_NODE : i ))
+
+	   get_layout "${nranks}"
     fi
+	lx=${m}
+	ly=${n}
+
+
+	get_layout "${i}"
+
 	ni=$(( 32 * ${m} ))
 	nj=$(( 32 * ${n} ))
 
@@ -74,22 +77,51 @@ LAYOUT = ${lx},${ly}
 #override DT_THERM = ${dt_therm}
 #override DT_FORCING = ${dt_therm}
 TIMEUNIT = ${dt}
-ENERGYSAVEDAYS = 75
+ENERGYSAVEDAYS = 50
 #override DAYMAX = 150
 EOF
+    # File label index
+    printf -v i0 "%03d" "$i"
+
     if [ "${PLATFORM}" == "gpu" ]; then
-        # TODO: GPU parallelization
-        nranks=1
+        # TODO: Flags must be built based on config!
+        ## GPU parallel (two per node)
+        #mpirun -np ${NGPUS} \
+        #    --map-by ppr:2:node \
+        #    --bind-to core \
+        #    bash -lc "
+        #        export CUDA_VISIBLE_DEVICES=\$OMPI_COMM_WORLD_LOCAL_RANK
+        #        exec ${MOM6_EXEC}
+        #    " \
+        #    | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
+
+        # GPU parallel (one per node)
+        mpirun -np ${NGPUS} --cpu-set ${first} \
+            --bind-to core \
+            bash -lc "
+                export CUDA_VISIBLE_DEVICES=\$OMPI_COMM_WORLD_LOCAL_RANK
+                exec ${MOM6_EXEC}
+            " \
+            | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
     else
-        nranks=${i}
+        nranks=$(( i > CPU_PER_NODE ? CPU_PER_NODE : i ))
+
+        mpirun -np ${nranks} \
+            --bind-to core \
+            ${MOM6_EXEC} \
+            | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
+        #mpirun -np ${nranks} \
+        #    --map-to socket \
+        #    --bind-to core \
+        #    ${MOM6_EXEC} \
+        #    | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
     fi
 
-    printf -v i0 "%03d" "$i"
-    mpirun -np ${nranks} ../build_omp/MOM6 \
-        | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
-
-    # TODO: what is the MPI GPU command?
-    #mpiexec -n 1 -x CUDA_VISIBLE_DEVICES=0 ../build_omp/MOM6 \
-    #    : -n 1 -x CUDA_VISIBLE_DEVICES=1 ../build_omp/MOM6 \
-    #| tee gpu_${i0}.out 2> gpu_${i0}.err
+    ## GPU parallel
+    #mpirun -np ${NGPUS} --cpu-set ${first} \
+    #    bash -lc "
+    #        export CUDA_VISIBLE_DEVICES=\$OMPI_COMM_WORLD_LOCAL_RANK
+    #        exec ${MOM6_EXEC}
+    #    " \
+    #    | tee ${PLATFORM}_${i0}.out 2> ${PLATFORM}_${i0}.err
 done
